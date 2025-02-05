72e373dcf2c9f60df47926dc92ea6e8cbd414ed3
kiranmayi1609 <Kiranmayi1609@gmail.com>
2025-02-05 14:01:20 +0100

    05.02

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
+  animations:[
+    trigger('shuttleSplitLeft', [
+      transition(':enter', [
+        animate('1200ms ease-in-out', keyframes([
+          style({ transform: 'translateX(-100%)', offset: 0 }),
+          style({ transform: 'translateX(10%)', offset: 0.7 }),
+          style({ transform: 'translateX(0)', offset: 1 })
+        ]))
+      ])
+    ]),
+    trigger('shuttleSplitRight', [
+      transition(':enter', [
+        animate('1200ms ease-in-out', keyframes([
+          style({ transform: 'translateX(100%)', offset: 0 }),
+          style({ transform: 'translateX(-10%)', offset: 0.7 }),
+          style({ transform: 'translateX(0)', offset: 1 })
+        ]))
+      ])
+    ])
+  ]
 })
+
 export class AboutComponent {
 
 }
