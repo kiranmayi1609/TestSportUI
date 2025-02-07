32d44c79d3d2e719f83e31a11f567b0a2bad8a75
kiranmayi1609 <Kiranmayi1609@gmail.com>
2025-02-07 10:16:50 +0100

    07.02

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,17 +5,19 @@ import { HomeComponent } from './home/home.component';
 import { TeamRegistrationComponent } from './team-registration/team-registration.component';
 import { ProfileFormComponent } from './profile-form/profile-form.component';
 import { AboutComponent } from './about/about.component';
-import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { InvoiceComponent } from './invoice/invoice.component';
 import { NewsComponent } from './news/news.component';
-import { TrainingComponent } from './training/training.component';
+
 import { TournmentComponent } from './tournment/tournment.component';
 import { CommunityComponent } from './community/community.component';
-import { SessionListComponent } from './session-list/session-list.component';
-import { SessionDetailsComponent } from './session-details/session-details.component';
-import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { SessionListComponent } from './training/session-list/session-list.component';
+import { SessionDetailsComponent } from './training/session-details/session-details.component';
+import { ProfileComponent } from './auth/profile/profile.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from '../dashboard/Member/dashboard.component';
+import { AdminDashboardComponent } from '../dashboard/admin-dashboard/admin-dashboard.component';
+
 
 export const routes: Routes = [
 
@@ -29,7 +31,7 @@ export const routes: Routes = [
   {path:'dashboard',component:DashboardComponent},
   {path:'admin',component:AdminDashboardComponent},
   {path:'invoice',component:InvoiceComponent},
-  {path:'training',component:TrainingComponent},
+  
   {path:'tournments',component:TournmentComponent},
   {path:'community',component:CommunityComponent},
   {path:'sessionList',component:SessionListComponent},
